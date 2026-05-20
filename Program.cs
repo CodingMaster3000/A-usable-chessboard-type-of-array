793a10358ad9538a7031d4aa7e89bbe4e8d5a9ec
CodingMaster3000 <67920617+CodingMaster3000@users.noreply.github.com>
2026-05-21 00:06:26 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace A_usable_chessboard_type_of_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
